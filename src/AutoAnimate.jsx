e80066891ea5ba40adef35daf4c6076a7d8094bb
Hunter Koppen <hunter.koppen@kobeon.nl>
2023-06-22 21:02:25 +0200

    Added icon and some settings

--- src/AutoAnimate.jsx
+++ src/AutoAnimate.jsx
@@ -1,10 +1,15 @@
 import { Component, createElement } from "react";
 
 import { AnimationContainer } from "./components/AnimationContainer";
-import "./ui/AutoAnimate.css";
 
 export class AutoAnimate extends Component {
     render() {
-        return <AnimationContainer content={this.props.content} />;
+        return (
+            <AnimationContainer
+                content={this.props.content}
+                duration={this.props.duration}
+                animationTiming={this.props.animationTiming}
+            />
+        );
     }
 }
--- src/components/AnimationContainer.jsx
+++ src/components/AnimationContainer.jsx
@@ -9,7 +9,29 @@ export class AnimationContainer extends Component {
 
     componentDidMount() {
         if (this.parentRef.current) {
-            autoAnimate(this.parentRef.current);
+            let easing;
+            switch (this.props.animationTiming) {
+                case "easeInOut":
+                    easing = "ease-in-out";
+                    break;
+                case "easeIn":
+                    easing = "ease-in";
+                    break;
+                case "easeOut":
+                    easing = "ease-out";
+                    break;
+                case "linear":
+                    easing = "linear";
+                    break;
+                default:
+                    easing = "ease-in-out";
+            }
+            autoAnimate(this.parentRef.current, {
+                // Animation duration in milliseconds (default: 250)
+                duration: this.props.duration,
+                // Easing for motion (default: 'ease-in-out')
+                easing: easing
+            });
         }
     }
 
